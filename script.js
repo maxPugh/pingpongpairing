2bb289acaa94fddd24bc079213289a85a20954c4
Mikail Alijev <mikail.alijev@illuminetsolutions.com>
2023-09-25 16:28:25 +0100

    Inheritance baybee

--- script.js
+++ script.js
@@ -16,6 +16,29 @@ class BankAccount {
   setBalance(balance) {
     this.balance = balance;
   }
+
+  withdraw(amount) {
+    this.balance -= amount;
+  }
+
+  deposit(amount) {
+    this.balance += amount;
+  }
+}
+
+class CustomerAccount extends BankAccount {
+  constructor(customerName, accountNumber, balance) {
+    super(customerName, accountNumber, balance);
+    this.overdraftLimit = -1000;
+  }
+
+  withdraw(amount) {
+    if (this.balance - amount > this.overdraftLimit) {
+      super.withdraw(amount);
+    } else {
+      throw new Error("Insufficient funds");
+    }
+  }
 }
 
 module.exports = BankAccount;
